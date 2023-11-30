5e7884396288c77c2d2f200a9034caeec6671962
PannaPro <pannacaliano@gmail.com>
2023-11-30 22:47:29 +0400

    added migration

--- database/migrations/2023_11_30_181651_create_posts_table.php
+++ database/migrations/2023_11_30_181651_create_posts_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            
+            $table->string('title');
+            $table->json('body')->nullable();
+
             $table->timestamps();
         });
     }
